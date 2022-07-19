8f7e180ade7862da5ea134abbdbe81685cbf5be7
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-07-19 09:52:46 -0300

    adding status bar

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
+import { Heading, VStack } from "native-base";
 
 export function SignIn() {
   return (
-    <View>
-      <Text>Olá Thiago</Text>
-    </View>
+    <VStack
+      flex={1}
+      alignItems="center"
+      bg="gray.600"
+      px={8}
+      pt={24}
+    >
+      <Heading
+        color="gray.100"
+        fontSize="xl"
+        mt={20}
+        mb={6}
+      >
+        Acesse sua conta
+      </Heading>
+
+    </VStack>
   )
 }
